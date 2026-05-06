5e1cadc968c9883e3c85e393d73dacebae21d81b
Кучерява Вікторія <kviktoria1710@gmail.com>
2026-05-06 23:47:56 +0300

    Connected the site to the database

---
+++ include/config.php
@@ -0,0 +1,6 @@
+$conn = mysqli_connect('localhost', 'root', '', 'shelter');
+if (mysqli_connect_errno()) {
+    echo 'Помилка підключення до БД: ' . mysqli_connect_error();
+    exit();
+}
+mysqli_set_charset($conn, "utf8mb4");
---
+++ include/credentials.example.php
@@ -0,0 +1,4 @@
+<?php
+// Приклад файлу конфігурації доступу адміністратора
+define('ADMIN_LOGIN', 'your_login');
+define('ADMIN_PASSWORD', 'your_password');
